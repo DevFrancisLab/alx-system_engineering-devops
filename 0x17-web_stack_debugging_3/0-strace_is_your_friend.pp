dbcdf189890bd11e4dd27e5242afa56ad3035ad9
DevFrancisLab <francmaki14@gmail.com>
2024-08-19 13:58:27 +0300

    Edit file to meet puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
-#Editing file
+#a puppet code that fixes a wordpress site 5xx error to 200 ok
+#editing the mistyped .phpp ot php in the /var/www/html/wp-settings.php file
 
 exec { 'fix-wordpress-server-error':
-    command => 'sed -i s/php/php/g /var/www/html/wp-settings.php',
-    path    => '/usr/bin/:/bin/',
+command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+path    => '/usr/bin/:/bin/',
 }
